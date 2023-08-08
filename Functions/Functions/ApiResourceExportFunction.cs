99f6c0a6a93c9f23a0492774a2da1f6c8b9df33e
Home <none@gmail.com>
2023-08-08 10:09:32 +0300

    initial_2

---
+++ Functions/Functions/ApiResourceExportFunction.cs
@@ -0,0 +1,30 @@
+using System.Net;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+
+namespace Functions.Functions
+{
+    public class ApiResourceExportFunction
+    {
+        private readonly ILogger _logger;
+
+        public ApiResourceExportFunction(ILoggerFactory loggerFactory)
+        {
+            _logger = loggerFactory.CreateLogger<ApiResourceExportFunction>();
+        }
+
+        [Function("ApiResourceExportFunction")]
+        public HttpResponseData Run([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
+        {
+            _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+            var response = req.CreateResponse(HttpStatusCode.OK);
+            response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
+
+            response.WriteString("Welcome to Azure Functions!");
+
+            return response;
+        }
+    }
+}
